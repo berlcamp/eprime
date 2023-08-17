a7f05982e863350c03298295fb8ca3b3dd11a47f
Berl <berlcamp@gmail.com>
2023-08-17 18:10:23 +0800

    ..

--- components/MainMenu.tsx
+++ components/MainMenu.tsx
@@ -4,11 +4,34 @@ import Link from 'next/link'
 
 const MainMenu: React.FC = () => {
   return (
-    <div className="py-1">
-      <div className='px-4 py-4'>
-        <div className='text-gray-700 text-xl font-semibold'>Menu</div>
-        <div className='lg:flex space-x-2'>
-          <div className='px-2 py-4 mt-2 lg:w-96 border text-gray-600 rounded-lg bg-white shadow-md flex flex-col space-y-2'>
+    <div className="py-1 relative">
+      <div className='px-6 mt-2 text-gray-700 text-xl font-semibold'>Menu</div>
+      <div className='px-4 py-2 overflow-y-auto h-[calc(100vh-170px)]'>
+        <div className='lg:flex lg:space-x-2 lg:space-y-0 space-y-2 justify-center lg:flex-row-reverse'>
+          <div className='px-2 py-4 border text-gray-600 rounded-lg bg-white shadow-md flex flex-col lg:mx-2 space-y-1'>
+            <div className='text-gray-700 text-lg font-semibold'>Shortcuts</div>
+            <Link href='/' className='app__menu_item'>
+              <PencilSquareIcon className='w-6 h-6'/>
+              <div className='app__menu_item_label'>Create Leave Request</div>
+            </Link>
+            <Link href='/' className='app__menu_item'>
+              <CreditCardIcon className='w-6 h-6'/>
+              <div className='app__menu_item_label'>My Leave Card</div>
+            </Link>
+            <Link href='/' className='app__menu_item'>
+              <TableCellsIcon className='w-6 h-6'/>
+              <div className='app__menu_item_label'>My Service Records</div>
+            </Link>
+            <Link href='/' className='app__menu_item'>
+              <ChartBarSquareIcon className='w-6 h-6'/>
+              <div className='app__menu_item_label'>PMS</div>
+            </Link>
+            <Link href='/' className='app__menu_item'>
+              <BriefcaseIcon className='w-6 h-6'/>
+              <div className='app__menu_item_label'>Vacant Items</div>
+            </Link>
+          </div>
+          <div className='px-2 py-4 lg:w-96 border text-gray-600 rounded-lg bg-white shadow-md flex flex-col space-y-2'>
             <div className='text-gray-700 text-lg font-semibold'>Human Resource</div>
             <Link href='/'>
               <div className='app__menu_item'>
@@ -116,29 +139,6 @@ const MainMenu: React.FC = () => {
                   <div className='app__menu_item_label_description'>System Access, Schools, Districts, Positions. </div>
                 </div>
               </div>
-            </Link>
-          </div>
-          <div className='px-2 py-4 mt-2 border text-gray-600 rounded-lg bg-white shadow-md flex flex-col space-y-1'>
-            <div className='text-gray-700 text-lg font-semibold'>Shortcuts</div>
-            <Link href='/' className='app__menu_item'>
-              <PencilSquareIcon className='w-6 h-6'/>
-              <div className='app__menu_item_label'>Create Leave Request</div>
-            </Link>
-            <Link href='/' className='app__menu_item'>
-              <CreditCardIcon className='w-6 h-6'/>
-              <div className='app__menu_item_label'>My Leave Card</div>
-            </Link>
-            <Link href='/' className='app__menu_item'>
-              <TableCellsIcon className='w-6 h-6'/>
-              <div className='app__menu_item_label'>My Service Records</div>
-            </Link>
-            <Link href='/' className='app__menu_item'>
-              <ChartBarSquareIcon className='w-6 h-6'/>
-              <div className='app__menu_item_label'>PMS</div>
-            </Link>
-            <Link href='/' className='app__menu_item'>
-              <BriefcaseIcon className='w-6 h-6'/>
-              <div className='app__menu_item_label'>Vacant Items</div>
             </Link>
           </div>
         </div>
