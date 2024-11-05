3340fd09eb592e5e0ee8c94de91fc2e47d4b7a6f
Berl treasure Campomanes <berlcamp@gmail.com>
2024-11-05 17:36:20 +0800

    ..added github cron

--- app/api/cronann/route.ts
+++ app/api/cronann/route.ts
@@ -1,5 +1,3 @@
-import { leaveCreditTypes } from '@/constants'
-import { Employee } from '@/types'
 import { createClient } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
 
@@ -14,55 +12,14 @@ export async function GET() {
     }
   })
 
-  try {
-    /*
-     * Automated CTO Expiration
-     */
-    const { data, error } = await supabase
-      .from('hrm_users')
-      .select()
-      .eq('status', 'Active')
+  // Trigger stored procedures
+  const { error: incrementError } = await supabase.rpc(
+    'increment_monthly_leave_credits'
+  )
 
-    if (error) {
-      throw new Error(error.message)
-    }
-
-    const currentYear = new Date().getFullYear() // Get the current year
-    const nextYear = currentYear + 1 // Add 1 to the current year
-    const resetDate = `${nextYear}/01/02`
-
-    const insertData: any = []
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
-    data.forEach(async (emp: Employee) => {
-      const leaveCardData = leaveCreditTypes.map((l) => {
-        return {
-          type: l.type,
-          gender: l.gender,
-          position_type: l.position_type,
-          date_of_next_reset:
-            l.type !== 'Vacation Leave' && l.type !== 'Sick Leave'
-              ? resetDate
-              : null,
-          credits: l.credits,
-          user_id: emp.id
-        }
-      })
-
-      insertData.push(...leaveCardData)
-    })
-
-    console.log('len: ', insertData.length)
-    // const { error: error3 } = await supabase
-    //   .from('hrm_leave_credits')
-    //   .insert(insertData)
-
-    // if (error3) {
-    //   throw new Error(error3.message)
-    // }
-
+  if (incrementError) {
+    return NextResponse.json(incrementError)
+  } else {
     return NextResponse.json('Cron completed')
-  } catch (error) {
-    console.log('Error: ', error)
-    return NextResponse.json(error)
   }
 }
