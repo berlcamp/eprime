e0944b18d4339e3df8cef0d8dcc0722d26f1aadb
Berl treasure Campomanes <berlcamp@gmail.com>
2024-11-19 21:06:54 +0800

    ..

--- app/(public)/vacant/page.tsx
+++ app/(public)/vacant/page.tsx
@@ -59,7 +59,7 @@ const Page: React.FC = () => {
                       href={`/rankingapplicantresults/ier?ref=${item.id}`}
                       className="app__btn_blue"
                     >
-                      IER
+                      Initial Evaluation Report
                     </Link>
                   </div>
                 </div>
